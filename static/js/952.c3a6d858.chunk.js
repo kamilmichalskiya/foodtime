a44f5faf8745dc3cf4e89e9a8bc6881de905ff7f
Kamil Michalski <kamil.michalski.ya@gmail.com>
2025-08-20 23:15:54 +0200

    Updates

---
+++ static/js/952.c3a6d858.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfoodtime=self.webpackChunkfoodtime||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.c3a6d858.chunk.js.map
